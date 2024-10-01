c46075d86b033cc5e827ccf43746e75476afc461
alexharrod <alexkharrod@gmail.com>
2024-10-01 07:40:44 -0400

    Card Flipping finished

--- cards.py
+++ cards.py
@@ -1,17 +1,9 @@
-from tkinter import Canvas, PhotoImage
-BACKGROUND_COLOR = "#B1DDC6"
-
-
-class Cards(Canvas):
-    def __init__(self):
-        super().__init__()
-        self.back = PhotoImage(file='./images/card_back.png')
-        self.front = PhotoImage(file='./images/card_front.png')
-        self.configure(width=800, height = 526, highlightthickness=0, bg=BACKGROUND_COLOR)
-        self.place(x=0, y=0)
 
 
 
-    def show_back(self):
-        self.create_image((400, 263), image=self.back)
+    def show_front(self,dic, rand_key):
+        self.text = dic["French"][rand_key]
+
+
+
 
--- main.py
+++ main.py
@@ -1,6 +1,29 @@
 from tkinter import *
-from cards import Cards
+import pandas as  pd
+import random
 
+# get data into pandas and convert to dict
+df = pd.read_csv("./data/french_words.csv")
+to_learn = df.to_dict('records')
+
+def show_back(word):
+    english_word = word["English"]
+    canvas.itemconfig(card_title, text="English")
+    canvas.itemconfig(card_word, text=english_word)
+
+def next_word():
+    """ picks a random number key from the dictionary"""
+
+    new_word = random.choice(to_learn)
+    french_word = new_word["French"]
+    canvas.itemconfig(card_title, text="French")
+    canvas.itemconfig(card_word, text=french_word)
+    window.after(3000, show_back, new_word)
+
+
+
+
+# update the canvas text with the associated value from pick_random for each lanage
 
 BACKGROUND_COLOR = "#B1DDC6"
 # setup gui
@@ -8,37 +31,26 @@ window = Tk()
 window.title("Flash Cards")
 window.config(width= 850, height=686,  padx=30, pady=50, bg=BACKGROUND_COLOR)
 
-# setup card canvas
-card = Cards()
-
-card.show_back()
-
-
-# setup Word Labels
-language_label = Label(text="French",bg=BACKGROUND_COLOR, fg="black",justify=LEFT,  font=("arial", 40, "italic"))
-language_label.place(x=330, y=150)
-
-word_label = Label(text="Word",bg=BACKGROUND_COLOR, fg="black",justify=LEFT, font=("arial", 60, "bold"))
-word_label.place(x=320, y=250)
-
-
 # setup buttons
 check_img = PhotoImage(file='./images/right.png')
-check_button = Button(width=90, height=90, bg=BACKGROUND_COLOR, image=check_img, compound=TOP, highlightthickness=0)
-check_button.place(x=580, y=530)
-
+right_button = Button(width=90, height=90, bg=BACKGROUND_COLOR, image=check_img, compound=TOP, highlightthickness=0, command=next_word)
+right_button.grid(row=1,column=1)
+#
 x_img = PhotoImage(file='./images/wrong.png')
 wrong_button = Button(width=90, height=90, bg=BACKGROUND_COLOR, image=x_img, compound=TOP, highlightthickness=0)
-wrong_button.place(x=180, y=530)
+wrong_button.grid(row=1,column=0)
 
+#setup canvas
+canvas = Canvas()
+back_image = PhotoImage(file='./images/card_back.png')
+front_image = PhotoImage(file='./images/card_front.png')
+canvas.configure(width=800, height=526, highlightthickness=0, bg=BACKGROUND_COLOR)
+canvas.create_image((400, 263), image=front_image)
+card_title = canvas.create_text((400, 150), text="", font=("arial", 40, "italic"), fill="black")
+card_word = canvas.create_text((400, 250), text="", font=("arial", 60, "normal"), fill="black")
+canvas.grid(row=0, column=0, columnspan=2)
 
-
-
-
-
-
-
-
+next_word()
 
 
 
